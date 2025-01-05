120122827a7dbd68fc384c5d7f90db3b18b95f8c
horiastate <horia.state@gmail.com>
2025-01-05 15:22:44 +0200

    update

--- class_chess_pieces.java
+++
@@ -1,3 +0,0 @@
-//obiecte pentru fiecare piesa de sah in parte
-//fiecare piesa de sah, dupa, se va misca pe matricea creata in partea de chess_board
-//toate clasele si desfasurarea jocului vor fi chemate in main_game.java
